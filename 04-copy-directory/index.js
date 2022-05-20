6e0fc4821086aa1153618ec6051023a1b8095d88
Nick Levshin <nikita.levshin2016@gmail.com>
2022-05-21 01:27:25 +0300

    feat: complete 04-copy-directory Сб 21 мая 2022 01:27:25 MSK

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir(dir, copy) {
+  const dirInfo = await fs.readdir(dir, { withFileTypes: true });
+
+  for (const file of dirInfo) {
+    if (file.isDirectory()) {
+      await fs.mkdir(path.resolve(copy, file.name));
+      await copyDir(
+        path.resolve(dir, file.name),
+        path.resolve(copy, file.name)
+      );
+    } else {
+      fs.copyFile(path.resolve(dir, file.name), path.resolve(copy, file.name));
+    }
+  }
+}
+
+fs.mkdir(path.resolve(__dirname, 'files-copy'), (err) => {
+  if (err) {
+    console.log('Error', err);
+  }
+});
+copyDir(
+  path.resolve(__dirname, 'files'),
+  path.resolve(__dirname, 'files-copy')
+);
